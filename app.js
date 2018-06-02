6b5a1b22d76f631bb7a99e604b67925c2d40e16e
SoonaM <SoonaM@Techtonic-MacBook-Air.local>
2018-06-01 20:33:56 -0700

    move the static file line

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static('client/build'));
 app.use('/users', usersRouter);
 app.use('/topics',topicsRouter);
-app.use(express.static('client/build'));
 
 
 // catch 404 and forward to error handler
