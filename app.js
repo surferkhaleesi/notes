a854595ef4a0f4e4b7cc429d40ef696fdd1794eb
SoonaM <SoonaM@Techtonic-MacBook-Air.local>
2018-06-01 19:29:44 -0700

    make client/build default route

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var topicsRouter = require('./routes/topics');
 
@@ -21,7 +20,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/topics',topicsRouter);
 app.use(express.static('client/build'));
